d52db939cd8699f081ebde1373d782db78cc04b4
athithyaramaa1 <149147655+athithyaramaa1@users.noreply.github.com>
2024-02-22 19:22:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,3 +25,17 @@ lass ReactContainer extends React.Component{
 const container = document.getElementById('react-container');
 ReactDOM.render(React.createElement(ReactContainer),container);
 
+ class ReactContainer extends React.Component{
+    // JSX tags
+    // JSX there should be one parent element
+      render(){
+          return (
+          <div>Hello! Welcome to Kalvium   
+             <div>This is babel</div>
+         </div> )
+      };  
+  }
+
+const container = document.getElementById('react-container');
+// Direclty pass the component to render
+ReactDOM.render(<ReactContainer/>,container); 
